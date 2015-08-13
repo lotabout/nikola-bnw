6ac042ed33e76e44e44adadd256e84a054799257
Zhang Jinzhou <zhang_ji@worksap.co.jp>
2015-08-13 11:30:42 +0800

    [header] add scroll event for header(show/hide).

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,6 +6,10 @@ $(function() {
       item[0].style.removeProperty('display');
       item.toggleClass('in');
     });
-
   });
 });
+
+// headroom
+var myElement = document.querySelector("header");
+var headroom  = new Headroom(myElement);
+headroom.init(); 
